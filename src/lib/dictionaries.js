9a318538e836c9e7d5f6919aa675ad7b0dd16555
Konstantin Neradovsky <konstantin.neradovskiy@open.ru>
2016-06-09 17:48:54 +0300

    dictionaries prepared for module use

--- src/lib/dictionaries.js
+++ src/lib/dictionaries.js
@@ -1,16 +1,27 @@
-function reduceDict(acttype,initial) {
-	const initialState = initial || [];
+
+function createReducer(acttype,initialState) {
 	return (state = initialState, action) => {
 		if(action.type!=acttype) return state;
 		return action.res.data;
 	};
 }
 
-export default function data_entitites(state = [], action) {
+function reduceObject(acttype,initial) {
+	const initialState = initial || {};
+	return createReducer(acttype,initialState);
+}
+
+function reduceArray(acttype,initial) {
+	const initialState = initial || [];
+	return createReducer(acttype,initialState);
+}
+
+export default function data_entitites(dataEntities) {
 	//generate entries for data entities
 	const entOpers = Object.keys(dataEntities).reduce((prev,cur)=> {
 		const res = Object.assign(prev);
-		res[cur+'s'] = reduceDict('LOAD_'+cur.toString().toUpperCase()+'S');
+		res[cur+'s'] = reduceArray('LOAD_'+cur.toString().toUpperCase()+'S');
+		res[cur] = reduceObject('GET_'+cur.toString().toUpperCase);
 		return res;
 	},{});
 	return entOpers;
